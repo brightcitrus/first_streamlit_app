7167b729ca6e70786287304b3abf2ae84d03cfba
brightcitrus <137465298+brightcitrus@users.noreply.github.com>
2023-06-23 03:15:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+    return fruityvice_normalized;
   
 streamlit.header("Fruityvice Fruit Advice!")
 
@@ -50,4 +50,4 @@ try:
       streamlit.error("Please select a fruit to get information.")
   else:
       back_from_function = get_fruityvice_data(fruit_choice)
-      streamlit.dataframe(back_from_function)
+      streamlit.dataframe(back_from_function);
